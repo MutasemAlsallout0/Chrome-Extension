748029af1cb45aebc1a9aa44c98830f544fc48d5
Mutasem Alsallout <mutasemalsallout@gmail.com>
2025-05-17 17:04:38 +0300

    Chrome Extension

---
+++ popup.js
@@ -0,0 +1,4 @@
+document.getElementById("sayHi").addEventListener("click", function () {
+  const output = document.getElementById("output");
+  output.textContent = "Hello from your Chrome Extension!";
+});
